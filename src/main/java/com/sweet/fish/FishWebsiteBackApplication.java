e5154909f7a409b2a156733692e846c477990f88
lingcuixue <cuixue.ling@casstime.com>
2022-02-16 18:06:55 +0800

    first commit

---
+++ src/main/java/com/sweet/fish/FishWebsiteBackApplication.java
@@ -0,0 +1,13 @@
+package com.sweet.fish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishWebsiteBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishWebsiteBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sweet/fish/FishWebsiteBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweet.fish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishWebsiteBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
